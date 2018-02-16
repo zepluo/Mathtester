509f356ed1e06e1e5b2b3b47ffee2e7d930b768c
MichelleTabansi <36417624+MichelleTabansi@users.noreply.github.com>
2018-02-16 11:14:37 -0600

    Merge branch 'master' of https://github.com/zepluo/Mathtester
    # Conflicts:
    #	Files/Questions/QuestionsFiles

--- src/DataStructure/Questions.java
+++ src/DataStructure/Questions.java
@@ -17,11 +17,19 @@ public class Questions {
     private String[] choices;
     private String correctAnswer;
     private String userAnswer;
+    private int type;
+    public static final int MULTIPLECHOICE=0;
+    public static final int FREERESPONSE = 1;
     
     
-    public Questions(int difficulty, String stem, String[] choices)
+    public Questions(int type, int difficulty, String stem, String[] choices, String correctAnswer)
     {
        this.difficulty = difficulty;
+       this.stem = stem;
+       this.choices = choices;
+       this.correctAnswer = correctAnswer;
+       this.type = type;
+       userAnswer = null;
     }
     
     public int getDifficulty()
@@ -29,9 +37,28 @@ public class Questions {
         return difficulty;
     }
     
+    public int getType()
+    {
+        return type;
+    }
     public String getStem()
     {
         return stem;
     }
+    
+    public String[] getchoices()
+    {
+        return choices;
+    }
             
+    public void setUserANswer( String Answer)
+    {
+        userAnswer = Answer;
+    }
+    
+    public String getUserAnswer()
+    {
+        return userAnswer;
+    }
+    
 }
